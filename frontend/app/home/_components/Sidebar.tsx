b652f9784b048f1f2fed396debaeac0a52f434c7
milinD537 <milindpithadia22@gmail.com>
2024-09-05 20:10:58 +0530

    feat: home structure re-worked, sidebar, navbar

--- frontend/app/home/_components/Sidebar.tsx
+++ frontend/app/home/_components/Sidebar.tsx
@@ -1,18 +1,20 @@
 "use client"
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { 
-  faFolder, 
-  faFileMedical, 
-  faFilePdf, 
-  faFileLines, 
-  faFileContract, 
-  faFileWord, 
-  faFilePen, 
-  faFileShield 
+import {
+  faFolder,
+  faFileMedical,
+  faFilePdf,
+  faFileLines,
+  faFileContract,
+  faFileWord,
+  faFilePen,
+  faFileShield
 } from '@fortawesome/free-solid-svg-icons';
 import { ChevronDown, Folder, ChevronRight, File } from 'lucide-react';
 import Link from 'next/link';
+import { div } from 'framer-motion/client';
+import { usePathname } from 'next/navigation';
 
 export type SidebarItemData = {
   name: string;
@@ -27,15 +29,18 @@ export type SidebarItemProps = {
 
 const SidebarItem: React.FC<SidebarItemProps> = ({ item, path = [] }) => {
   const [isOpen, setIsOpen] = useState(false);
-
+  const pathname = usePathname();
+  
   const hasChildren = item.children && item.children.length > 0;
   const currentPath = [...path, item.name.toLowerCase().replace(/\s+/g, '-')];
   const href = hasChildren ? '#' : `/home/${currentPath.join('/')}`;
+  const isActive = pathname === href || pathname.startsWith(`${href}/`);
 
   return (
-    <li>
+    <li className={`${!hasChildren ? "grid gap-1 grid-cols-[2px_1fr]" : "mt-2"}`}>
+      {!hasChildren && <div className={`bg-primary ${!isActive && "bg-opacity-50"} transition-colors`}></div>}
       <Link href={href}
-        className={`flex items-center p-2 text-gray-300 rounded-lg hover:bg-gray-700 group ${hasChildren ? 'font-bold' : 'text-sm'}`}
+        className={`flex items-center gap-1.5 p-2 text-gray-300 srounded-lg hover:bg-[hsl(260,100%,83%)] group ${isActive && "bg-[hsl(260,100%,83%)]"} ${hasChildren ? 'font-bold' : 'text-sm m-0.5'}`}
         onClick={(e) => {
           if (hasChildren) {
             e.preventDefault();
@@ -49,11 +54,11 @@ const SidebarItem: React.FC<SidebarItemProps> = ({ item, path = [] }) => {
             {isOpen ? <ChevronDown className="w-5 h-5" /> : <ChevronRight className="w-5 h-5" />}
           </span>
         )}
-        {hasChildren ? <Folder className="w-5 h-5" /> : <File className="w-5 h-5" />}
-        <span className="flex-1 ms-3 whitespace-nowrap">{item.name}</span>
+        {hasChildren ? <Folder className="h-full p-0.5" /> : <File className="h-5 p-0.5 pe-0" />}
+        <span className="flex-1 whitespace-nowrap">{item.name}</span>
       </Link>
       {isOpen && hasChildren && (
-        <ul className="pl-4 mt-2 space-y-2">
+        <ul className="pl-4 mt-2">
           {item.children?.map((child, index) => (
             <SidebarItem key={index} item={child} path={currentPath} />
           ))}
@@ -69,8 +74,8 @@ export type SidebarProps = {
 
 const Sidebar: React.FC<SidebarProps> = ({ data }) => {
   return (
-    <aside id="logo-sidebar" className="fixed bg-[hsl(210,3%,23%)] top-0 left-0 z-40 w-80 h-screen pt-20 transition-transform -translate-x-full border-r border-gray-700 lg:translate-x-0" aria-label="Sidebar">
-      <div className="scrollbar | h-full px-3 pb-4 overflow-y-auto">
+    <aside id="logo-sidebar" className="bg-[hsl(260,100%,93%)] pt-14 h-screen overflow-hidden shadow-md" aria-label="Sidebar">
+      <div className="scrollbar | h-full overflow-y-auto p-3">
         <ul className="space-y-2 font-medium">
           {data.map((item, index) => (
             <SidebarItem key={index} item={item} />
--- frontend/app/home/layout.tsx
+++ frontend/app/home/layout.tsx
@@ -8,7 +8,7 @@ const HomeLayout = ({ children }: { children: React.ReactNode }) => {
     return (
         <main className="h-full">
             <Navbar />
-            <div className="h-full flex-1">
+            <div className="grid grid-cols-[0rem_1fr] lg:grid-cols-[20rem_1fr] transition-[grid-template-columns] duration-500">
                 <Sidebar data={sidebarData.sidebarData} />
                 {children}
             </div>
