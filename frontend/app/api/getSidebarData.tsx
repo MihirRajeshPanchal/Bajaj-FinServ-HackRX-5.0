f548e92dfd2b8da486dfbad21cb023e0041f6100
parshav92 <dedhiaparshav@gmail.com>
2024-09-09 00:09:11 +0530

    chore: Refactor sidebarData component and loginButton component

---
+++ frontend/app/api/getSidebarData.tsx
@@ -0,0 +1,10 @@
+import { SidebarItemData } from "../home/_components/sidebar";
+
+export async function fetchSidebarData(): Promise<SidebarItemData[]> {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/frontend_sidebar_json`, { cache: 'force-cache' });
+    if (!res.ok) {
+      throw new Error('Failed to fetch sidebar data');
+    }
+    const data = await res.json();
+    return data.sidebarData; 
+  }
--- frontend/components/loginButton.tsx
+++ frontend/components/loginButton.tsx
@@ -1,63 +1,36 @@
 'use client';
-
 import { useState, useEffect } from 'react';
-import { setCookie, getCookie, deleteCookie } from 'cookies-next';
+import { getCookie } from 'cookies-next';
 import { useRouter } from 'next/navigation';
-import { toast, Toaster } from 'sonner';
+import { Toaster } from 'sonner';
+import getAuth from '@/app/api/getAuth';
 
 const LoginButton = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-    const router = useRouter();
+  const router = useRouter();
+
   useEffect(() => {
     const email = getCookie('email');
     setIsAuthenticated(!!email);
   }, []);
 
-  const handleAuth = async () => {
-    if (isAuthenticated) {
-      deleteCookie('email');
-      toast.success('Logged out');
-      setIsAuthenticated(false);
-    } else {
-      try {
-        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth`, {
-          method: 'GET',
-          credentials: 'include',
-        });
-
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-
-        const data = await response.json();
-
-        if (data.response === "Authorization Successful") {
-            toast.success('Authentication successful');
-          setCookie('email', data.email);
-          setIsAuthenticated(true);
-          router.push('/home');
-          
-        } else {
-          console.error('Authentication failed');
-        }
-      } catch (error) {
-        toast.error('Authentication failed');
-        console.error('Error during authentication:', error);
-      }
-    }
+  const handleAuth = () => {
+    getAuth({
+      isAuthenticated,
+      setIsAuthenticated,
+      onLoginSuccess: () => router.push('/home')
+    });
   };
 
   return (
     <div>
-
-    
-    <Toaster />
-    <button
-      onClick={handleAuth}
-      className="py-1 px-6 bg-accent font-medium text-text shadow-[4px_4px_black] -translate-x-0.5 -translate-y-0.5 hover:translate-x-0 hover:translate-y-0 hover:shadow-[0px_0px_black] transition-[transform,box-shadow]"
-    >
-      {isAuthenticated ? 'Logout' : 'Login'}
-    </button>
+      <Toaster />
+      <button
+        onClick={handleAuth}
+        className="py-1 px-6 bg-accent font-medium text-text shadow-[4px_4px_black] -translate-x-0.5 -translate-y-0.5 hover:translate-x-0 hover:translate-y-0 hover:shadow-[0px_0px_black] transition-[transform,box-shadow]"
+      >
+        {isAuthenticated ? 'Logout' : 'Login'}
+      </button>
     </div>
   );
 };
